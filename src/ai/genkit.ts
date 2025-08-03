83780242f6df8a84511616c38d9ebb1a0254ae31
Marcelo Felix <mocfelix@gmail.com>
2025-08-03 21:03:18 +0000

    ild Error
    Export default doesn't exist in target module
    
    ./src/ai/genk

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,7 @@
+'use server';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import firebase from '@genkit-ai/firebase';
+import {firebase} from '@genkit-ai/firebase';
 
 export const ai = genkit({
   plugins: [
